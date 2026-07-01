9ea69e380f865bb1c60b331aa9f8fac99f7fe928
Krishna <krishnaparajuli331@gmail.com>
2026-07-01 08:48:32 +0545

    extra 2.0

--- applications/node-service/src/server.js
+++ applications/node-service/src/server.js
@@ -10,7 +10,15 @@ const employeeRoutes = require("./routes/employee");
 
 app.use("/employees",employeeRoutes);
 
-
+app.get("/version", (req, res) => {
+    res.json({
+        application: "Enterprise DevOps Platform",
+        version: "2.0",
+        deployedBy: "Jenkins",
+        deployedAt: new Date(),
+        author: "Krishna"
+    });
+});
 
 app.listen(5000,()=>{
 
